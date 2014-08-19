83256b2d8407d45ee1cd19a5c25408b3575787ea
Vincent Weevers <mail@vincentweevers.nl>
2014-08-19 19:31:19 +0200

    get-image-data fork

--- test/server.js
+++ test/server.js
@@ -1,7 +1,10 @@
 var huey = require('../')
+var fs = require('fs')
 
-require('./shared')(huey, {
-  trad: __dirname + '/images/trad.jpg',
-  jerry: __dirname + '/images/jerry.jpg',
-  astronaut: __dirname + '/images/astronaut.jpg'
+fs.readFile(__dirname + '/images/astronaut.jpg', function(err, buf){
+  require('./shared')(huey, {
+    trad: __dirname + '/images/trad.jpg',
+    jerry: __dirname + '/images/jerry.jpg',
+    astronaut: buf
+  })
 })
